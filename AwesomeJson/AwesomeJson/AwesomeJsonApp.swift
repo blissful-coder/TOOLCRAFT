0c1178ce15e7f1830faa57495b4c13e795912d3d
blissful_coder <anurag.negi1729@gmail.com>
2024-02-09 11:10:40 +0530

    code(delete): removed unused files

---
+++ AwesomeJson/AwesomeJson/AwesomeJsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwesomeJsonApp.swift
+//  AwesomeJson
+//
+//  Created by Negi, Anurag on 08/02/24.
+//
+
+import SwiftUI
+
+@main
+struct AwesomeJsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
